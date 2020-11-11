a5560c53bb490572397b5d69530b20937e0a1a47
Dumagori <iandrusenko1099@gmail.com>
2020-11-11 21:12:11 +0200

    Laba 4

---
+++ src/test/java/laba4/UniversityWriterTest.java
@@ -0,0 +1,16 @@
+package laba4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UniversityWriterTest {
+
+    @Test
+    public void writeToFile() {
+    }
+
+    @Test
+    public void readFromFile() {
+    }
+}
